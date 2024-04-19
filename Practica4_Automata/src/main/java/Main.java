ae33bc9f12b5e336f92288843ad08ed998a7fb02
jusancc <jusancc@unirioja.es>
2024-04-19 13:33:09 +0200

    19/04

--- Practica4_Automata/src/main/java/Main.java
+++ Practica4_Automata/src/main/java/Main.java
@@ -4,64 +4,31 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, String> tokens = new HashMap<>();
-        tokens.put(0, "cero");
-        tokens.put(1, "uno");
-        tokens.put(2, "dos");
-        tokens.put(3, "tres");
-        tokens.put(4, "cuatro");
+        // Definición de las equivalencias de tokens
+        Map<Integer, String> equivTokens = new HashMap<>();
+        equivTokens.put(0, "cero");
+        equivTokens.put(1, "uno");
+        equivTokens.put(2, "dos");
+        equivTokens.put(3, "tres");
+        equivTokens.put(4, "cuatro");
 
-        // Autómata para el patrón 'a'
-        AutomataFinitoMatriz automataA = new AutomataFinitoMatriz(2, 3);
-        automataA.setFinales(new boolean[]{false, true});
-        automataA.transicion(0, 0);
-        automataA.transicion(1, 0);
-        automataA.transicion(0, 0);
-        automataA.transicion(1, 0);
+        // Definición de la matriz de transiciones para el autómata finito
+        int[][] matriz = {
+                {1, -1, -1},   // Estado 0: Transiciones para a, b, c
+                {-1, 2, -1},   // Estado 1: Transiciones para a, b, c
+                {-1, -1, 3}    // Estado 2: Transiciones para a, b, c
+        };
 
-// Autómata para el patrón 'b'
-        AutomataFinitoMatriz automataB = new AutomataFinitoMatriz(2, 3);
-        automataB.setFinales(new boolean[]{false, true});
-        automataB.transicion(0, 1);
-        automataB.transicion(1, 1);
-        automataB.transicion(0, 1);
-        automataB.transicion(1, 1);
+        // Creación del autómata finito
+        AutomataFinito automata = new AutomataFinitoMatriz(3, 3, matriz, new boolean[]{false, false, true});
 
-// Autómata para el patrón 'aabb'
-        AutomataFinitoMatriz automataAABB = new AutomataFinitoMatriz(4, 3);
-        automataAABB.setFinales(new boolean[]{false, false, false, true});
-        automataAABB.transicion(0, 0);
-        automataAABB.transicion(1, 0);
-        automataAABB.transicion(2, 0);
-        automataAABB.transicion(3, 0);
-        automataAABB.transicion(3, 1);
-        automataAABB.transicion(0, 1);
-        automataAABB.transicion(1, 1);
-        automataAABB.transicion(2, 1);
-
-// Autómata para el patrón '((a)*)((b)+)'
-        AutomataFinitoMatriz automataAB = new AutomataFinitoMatriz(4, 3);
-        automataAB.setFinales(new boolean[]{true, false, false, false});
-        automataAB.transicion(0, 0);
-        automataAB.transicion(1, 0);
-        automataAB.transicion(2, 0);
-        automataAB.transicion(3, 0);
-        automataAB.transicion(3, 1);
-        automataAB.transicion(0, 1);
-        automataAB.transicion(1, 1);
-        automataAB.transicion(2, 1);
-
-// Autómata para el patrón '(c)+'
-        AutomataFinitoMatriz automataC = new AutomataFinitoMatriz(2, 3);
-        automataC.setFinales(new boolean[]{false, true});
-        automataC.transicion(0, 2);
-        automataC.transicion(1, 2);
-        automataC.transicion(0, 2);
-        automataC.transicion(1, 2);
-
+        // Cadena de entrada
         int[] cadena = {0, 1, 0, 1, 1, 2, 2, 0, 2, 2, 2};
-        AnalizadorLexico analizador = new AnalizadorLexico(cadena, automataAABB, tokens);
 
+        // Creación del analizador léxico
+        AnalizadorLexico analizador = new AnalizadorLexico(cadena, automata, equivTokens);
+
+        // Obtención de los tokens
         Token token;
         while ((token = analizador.nextToken()) != null) {
             System.out.println("Token: " + token.getId() + ", Lexema: " + Arrays.toString(token.getLexema()));
